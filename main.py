336e405c6c09b11f9859d79c9a78265b03af8865
Queensly Kyerewaa Acheampongmaa <qacheampong@gmail.com>
2023-05-26 14:03:08 +0000

    Updated

--- main.py
+++ main.py
@@ -4,23 +4,24 @@ import pickle
 import pandas as pd
 import uvicorn
 
-#function to load pickle file
+app = FastAPI()
+
+# Function to load pickle file
 def load_pickle(filename):
     with open(filename, 'rb') as file:
         data = pickle.load(file)
         return data
 
+# Load pickle file
+ml_components = load_pickle('sepsis.pkl')
 
-#load  pickle file
-ml_components= load_pickle('sepsis.pkl')
-
-#components in pickle file
+# Components in the pickle file
 ml_model = ml_components['model']
 full_pipeline = ml_components['pipeline']
-num_pipeline =ml_components['num_pipe']
+num_pipeline = ml_components['num_pipe']
 
 # Setup the BaseModel
-class ModelInput(BaseModel):
+class Input(BaseModel):
     Plasma_glucose: int
     Blood_Work_Result_1: int
     Blood_Pressure: int
@@ -30,73 +31,39 @@ class ModelInput(BaseModel):
     Blood_Work_Result_4: float
     Age: int
 
-def make_prediction(Plasma_glucose, Blood_Work_Result_1, Blood_Pressure, Blood_Work_Result_2, Blood_Work_Result_3, Body_mass_index, Blood_Work_Result_4, Age):
-    df = pd.DataFrame([['Plasma glucose', 'Blood Work Result-1', 'Blood Pressure', 'Blood Work Result-2', 'Blood Work Result-3', 'Body mass index', 'Blood Work Result-4', 'Age']],
-                      columns = ['Plasma glucose', 'Blood Work Result-1', 'Blood Pressure', 'Blood Work Result-2', 'Blood Work Result-3', 'Body mass index', 'Blood Work Result-4', 'Age']
-                      )
-    
+def make_prediction(data):
+    # Create a DataFrame from the input data
+    df = pd.DataFrame(data)
+
     # Perform transformations using the full_pipeline
-    df_prepared= full_pipeline.transform(df)
+    df_prepared = full_pipeline.transform(df)
 
     # Make the prediction and return output
     model_output = ml_model.predict(df_prepared).tolist()
     return model_output
 
-# Endpoints
-@app.post("/Sepsis Status")
-async def predict(input: ModelInput):
-    output_pred = make_prediction(
-        Plasma_glucose= input.Plasma_glucose,
-        Blood_Work_Result-1= input.Blood_Work_Result-1,
-        Blood_Pressure=input.Blood_Pressure,
-        Blood_Work_Result-2=input.Blood_Work_Result-2,
-        Blood_Work_Result-3=input.Blood_Work_Result-3,
-        Body_mass_index= input.Body_mass_index,
-        Blood_Work_Result-4=input.Blood_Work_Result-4,
-        Age=input.Age
-       
-        )
+# Endpoint
+@app.post("/predict")
+def predict_sepsis(input_data:Input):
+    # Convert the input data to a dictionary
+    input_dict = input_data.dict()
 
-    # Labelling Model output
-    if output_pred == 0:
+    # Make the prediction
+    prediction = make_prediction([input_dict])
+
+    # Label the model output
+    if prediction[0] == 0:
         output_pred = "Sepsis status is Negative"
     else:
         output_pred = "Sepsis status is Positive"
-    #return output_pred
-    return {"prediction": output_pred,
-            "input": input
-            }
 
-# Set the API to run
-if __name__ == "__main__":
-    uvicorn.run("api:app",
-                reload=True
-                )    
-    
-# from typing import Optional
-
-# class Package(BaseModel):
-#     name:str
-#     number:str
-#     description:Optional[str]=None
-
-# app = FastAPI()
-
-# @app.get('/')
-
-# async def hello_world():
-#     return {'hello':'world'}
-
-# @app.post('/package/(priority)')
-# async def make_package(priority: int, package:Package, value:bool):
-#     return {'priority':priority, **package.dict(), 'value':value}
+    return {"prediction": output_pred, "input": input_data.dict()}
 
 
-# @app.get('/component/(component_id)')    #path parameter
-# async def get_component(component_id :int):
-#     return {'component_id':component_id}
 
-# @app.get('/component/')    #query parameter
-# async def read_component(number :int, text:str):
-#     return {'number':number, 'text':text}
+
+
+
+
+
 
