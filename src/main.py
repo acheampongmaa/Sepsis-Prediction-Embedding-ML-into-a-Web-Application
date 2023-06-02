c183f801c44d8d22f079ac0f6bf3fc6e19f014e6
Queensly Kyerewaa Acheampongmaa <qacheampong@gmail.com>
2023-06-02 01:19:41 +0000

    Updated codes

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@ def load_pickle(filename):
     
 
 # Load pickle file
-ml_components = load_pickle('src\assets\sepsis.pkl')
+ml_components = load_pickle('src/assets/sepsis.pkl') 
 
 # Components in the pickle file
 ml_model = ml_components['model']
@@ -27,55 +27,95 @@ app = FastAPI()
 
 
 # Setup the BaseModel
-class ModelInput(BaseModel):
-    Plasma_glucose: int
-    Blood_Work_Result_1: int
-    Blood_Pressure: int
-    Blood_Work_Result_2: int
-    Blood_Work_Result_3: int
-    Body_mass_index: float
-    Blood_Work_Result_4: float
-    Age: int
+# class ModelInput(BaseModel):
+#     Plasma_glucose: int
+#     Blood_Work_Result_1: int
+#     Blood_Pressure: int
+#     Blood_Work_Result_2: int
+#     Blood_Work_Result_3: int
+#     Body_mass_index: float
+#     Blood_Work_Result_4: float
+#     Age: int
+
+# data = pd.DataFrame({'Plasma glucose': ['Plasma_glucose'], 'Blood Work Result-1':	['Blood_Work_Result_1'],
+#                          'Blood Pressure': ['Blood_Pressure'], 'Blood Work Result-2': ['Blood_Work_Result_2'],
+#                         'Blood Work Result-3': ['Blood_Work_Result_3'], 'Body mass index': ['Body_mass_index'],
+#                         'Blood Work Result-4':	['Blood_Work_Result_4'], 'Age': ['Age']})
+
+@app.get('/Predict Sepsis')
+async def predict(Plasma_glucose: int, Blood_Work_Result_1: int,
+                  Blood_Pressure: int, Blood_Work_Result_2: int,
+                    Blood_Work_Result_3: int, Body_mass_index: float, 
+                    Blood_Work_Result_4: float,Age: int ):
     
-
-
-def make_prediction(Plasma_glucose, Blood_Work_Result_1, Blood_Pressure, 
-                    Blood_Work_Result_2, Blood_Work_Result_3, Body_mass_index, 
-                    Blood_Work_Result_4, Age):
     data = pd.DataFrame({'Plasma glucose': [Plasma_glucose], 'Blood Work Result-1':	[Blood_Work_Result_1],
                          'Blood Pressure': [Blood_Pressure], 'Blood Work Result-2': [Blood_Work_Result_2],
                         'Blood Work Result-3': [Blood_Work_Result_3], 'Body mass index': [Body_mass_index],
                         'Blood Work Result-4':	[Blood_Work_Result_4], 'Age': [Age]})
-   
     
-
     data_prepared = full_pipeline.transform(data)
 
-    # Make the prediction and return output
     model_output = ml_model.predict(data_prepared).tolist()
+
     return model_output
 
-# Endpoints
-@app.post("/Sepsis")
-async def predict(input: ModelInput):
-    output_pred = make_prediction(
-        Plasma_glucose=input.Plasma_glucose,
-        Blood_Work_Result_1=input.Blood_Work_Result_1,
-        Blood_Pressure=input.Blood_Pressure,
-        Blood_Work_Result_2=input.Blood_Work_Result_2,
-        Blood_Work_Result_3=input.Blood_Work_Result_3,
-        Body_mass_index=input.Body_mass_index,
-        Blood_Work_Result_4=input.Blood_Work_Result_4,
-        Age=input.Age
-        )
+def make_prediction():
 
-    # Labelling Model output
+    output_pred = make_prediction()
+
     if output_pred == 0:
         output_pred = "Sepsis status is Negative"
     else:
         output_pred = "Sepsis status is Positive"
-    #return output_pred
-    return {"prediction": output_pred,
-            "input": input
-            }
+        
+        return {"prediction": output_pred}
 
+
+
+# def make_prediction(Plasma_glucose, Blood_Work_Result_1, Blood_Pressure, 
+#                     Blood_Work_Result_2, Blood_Work_Result_3, Body_mass_index, 
+#                     Blood_Work_Result_4, Age):
+# data = pd.DataFrame({'Plasma glucose': [Plasma_glucose], 'Blood Work Result-1':	[Blood_Work_Result_1],
+#                          'Blood Pressure': [Blood_Pressure], 'Blood Work Result-2': [Blood_Work_Result_2],
+#                         'Blood Work Result-3': [Blood_Work_Result_3], 'Body mass index': [Body_mass_index],
+#                         'Blood Work Result-4':	[Blood_Work_Result_4], 'Age': [Age]})
+# def make_prediction():
+    # data_prepared = full_pipeline.transform(data)
+
+    # Make the prediction and return output
+    # model_output = ml_model.predict(data_prepared).tolist()
+
+    # if output_pred == 0:
+    #     output_pred = "Sepsis status is Negative"
+    # else:
+    #     output_pred = "Sepsis status is Positive"
+
+    # return model_output
+    
+
+# Endpoints
+# @app.post("/Sepsis")
+# async def predict(input: ModelInput):
+#     output_pred = make_prediction(
+#         Plasma_glucose=input.Plasma_glucose,
+#         Blood_Work_Result_1=input.Blood_Work_Result_1,
+#         Blood_Pressure=input.Blood_Pressure,
+#         Blood_Work_Result_2=input.Blood_Work_Result_2,
+#         Blood_Work_Result_3=input.Blood_Work_Result_3,
+#         Body_mass_index=input.Body_mass_index,
+#         Blood_Work_Result_4=input.Blood_Work_Result_4,
+#         Age=input.Age
+#         )
+
+    # Labelling Model output
+    # if output_pred == 0:
+    #     output_pred = "Sepsis status is Negative"
+    # else:
+    #     output_pred = "Sepsis status is Positive"
+    # #return output_pred
+    # return {"prediction": output_pred,
+    #         "input": input
+    #         }
+
+if __name__=='__main__':
+    uvicorn.run('main:app', reload=True)
