ea3e60482a3ac9bc3d36727aaf2957ddee254904
Queensly Kyerewaa Acheampongmaa <qacheampong@gmail.com>
2023-06-03 18:29:13 +0000

    Updated codes

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 import pandas as pd
 import os
 import sys
-
+import numpy as np
 
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
